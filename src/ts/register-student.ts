52b796e55acb77e20c7423c556c4261d991a8d85
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-04-14 10:18:10 +0530

    Modified the validation part of the register-students

--- src/ts/register-student.ts
+++ src/ts/register-student.ts
@@ -1,6 +1,32 @@
 let txtName: HTMLInputElement =document.querySelector("#txt-studentName")!;
 let txtNIC: HTMLInputElement =document.querySelector("#txt-studentNic")!;
 let txtEmail: HTMLInputElement=document.querySelector("#txt-studentEmail")!;
+let frmRegistration:HTMLFormElement =document.querySelector('#frm-register-students')!;
+
+frmRegistration.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    let inputs:HTMLInputElement[]=[txtName,txtNIC,txtEmail];
+    let invalidInputElm=inputs.find((input:HTMLInputElement)=>
+        input.classList.contains('is-invalid') || input.value.length===0
+    );
+    if (invalidInputElm){
+        invalidInputElm.classList.add('is-invalid');
+        invalidInputElm.focus();
+        invalidInputElm.select();
+        return;
+    }else {
+        let student={
+            name:txtName.value.trim(),
+            nic:txtNIC.value.trim(),
+            email:txtEmail.value.trim()
+        };
+        let studentJSON=JSON.stringify(student);
+        console.log(studentJSON);
+
+    }
+});
+
 
 const validationListner =(e:Event)=>{
     if (e.target instanceof HTMLInputElement){
