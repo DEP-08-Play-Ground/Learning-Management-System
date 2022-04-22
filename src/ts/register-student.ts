b1c4257608f00af89f861a0a28b7321c79f5c383
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-04-22 19:24:22 +0530

    Add AJAX codes to the frontend

--- src/ts/register-student.ts
+++ src/ts/register-student.ts
@@ -4,12 +4,13 @@ let txtEmail: HTMLInputElement=document.querySelector("#txt-studentEmail")!;
 let frmRegistration:HTMLFormElement =document.querySelector('#frm-register-students')!;
 
 frmRegistration.addEventListener('submit',(e)=>{
-    e.preventDefault();
+    e.preventDefault(); /*to stop the default behaviour of a submit button*/
 
     let inputs:HTMLInputElement[]=[txtName,txtNIC,txtEmail];
     let invalidInputElm=inputs.find((input:HTMLInputElement)=>
         input.classList.contains('is-invalid') || input.value.length===0
     );
+
     if (invalidInputElm){
         invalidInputElm.classList.add('is-invalid');
         invalidInputElm.focus();
@@ -23,6 +24,33 @@ frmRegistration.addEventListener('submit',(e)=>{
         };
         let studentJSON=JSON.stringify(student);
         console.log(studentJSON);
+
+        /*XMLHttpRequest*/
+        let http = new XMLHttpRequest();
+
+        /*Let's catch the response*/
+
+        http.onreadystatechange =() =>{
+          //this is going to work, when the response is available
+          // console.log("response eka aawaaaa.!");
+          // console.log(http.readyState,http.status);
+            if (http.readyState===XMLHttpRequest.DONE){
+                if (http.status===201){
+                    console.log(http.responseText);
+                }else {
+                    console.log("Failed to save the student!")
+                }
+            }
+        };
+
+        /* Let's initiate the request*/
+        http.open('POST','http://localhost:8080/lms/students',true);
+
+        /*Let's set some request*/
+        http.setRequestHeader("Content-Type","application/json");
+
+        /*Let's send the request*/
+        http.send(studentJSON);
 
     }
 });
@@ -45,6 +73,7 @@ const validationListner =(e:Event)=>{
         }else {
             e.target.classList.add('is-invalid');
         }
+
     }
 }
 txtName.addEventListener('input',validationListner);
